1a704ae6ae3054c80305b468af32e5b9c869b291
Determinant <ted.sybil@gmail.com>
2015-11-01 00:37:36 +0800

    add waiting message

--- themes/ekg/src/js/gphotos.js
+++ themes/ekg/src/js/gphotos.js
@@ -68,18 +68,65 @@ var openPhotoSwipe = function(index, items, galleryElement, disableAnimation, fr
     gallery.init();
 };
 
+function block_element_and_wait(selector) {
+    var note = document.createElement('div');
+    var overlay = document.createElement('div');
+    /* var height = Math.max(selector.height(), 200); */
+    var height = selector.height();
+    var width = selector.width();
+    var pos = selector.position();
+    var text = document.createElement('span');
+    var spinner = document.createElement('span');
+
+    text.innerHTML = "Please wait...";
+    text.className = "blocknotetext";
+
+    spinner.className = "glyphicon glyphicon-refresh spin";
+
+    selector.toggleClass('blocked', true);
+    note.className = "blocknote";
+    note.style.top = (height - 50) / 2 + 'px';
+    note.style.left = (width - 200) / 2 + 'px';
+    note.appendChild(spinner);
+    note.appendChild(text);
+
+    overlay.className = "blockoverlay";
+    overlay.style.top = pos.top + 'px';
+    overlay.style.left = pos.left + 'px';
+    overlay.style.height = height + 'px';
+    overlay.style.width = width + 'px';
+    overlay.appendChild(note);
+
+    selector.after(overlay);
+    selector.data('_blockoverlay', overlay);
+    selector.data('_blocknote', note);
+    selector.data('_blocked', true);
+}
+
+function unblock_element_and_finish(selector) {
+    if (selector.data('_blocked'))
+    {
+        $(selector.data('_blockoverlay')).remove();
+        $(selector.data('_blocknote')).remove();
+        selector.data('_blocked', undefined);
+        selector.toggleClass('blocked', false);
+    }
+}
+
 var google_uid = "108853535017808641401";
 var google_aid = "6211742483741396737";
 var column_width = 200;
 var max_results = 999;
 
 $(function () {
+    block_element_and_wait($('#gphotos'));
     $.ajax({url: "http://picasaweb.google.com/data/feed/api/user/" + google_uid +
            "/albumid/" + google_aid +
                "?imgmax=1600" +
                "&max-results=" + max_results +
                "&start-index=1&alt=json",
         crossDomain: true,
+        timeout: 10000,
         success: function (resp) {
            var container = document.getElementsByClassName("gphotos")[0];
            var items = [];
@@ -127,8 +174,14 @@ $(function () {
             gutterX: 5,
             gutterY: 5
         });
+        unblock_element_and_finish($('#gphotos'));
         wall.container.find('.item img').load(function() {
              wall.fitWidth();
         });
-    }});
+        },
+        error: function () {
+            console.log('failed');
+            setTimeout(function () { $.ajax(this); }, 2000);
+        }
+    });
 });
